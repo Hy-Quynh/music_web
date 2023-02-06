fc36d2d4e89a669f326535e10b816527878ded6f
Hy-Quynh <hyquynh123@gmail.com>
2023-02-06 22:25:21 +0700

    config private router v2

--- client/src/routers/PrivateRouter/AdminPrivateRouter.js
+++ client/src/routers/PrivateRouter/AdminPrivateRouter.js
@@ -5,9 +5,7 @@ import { USER_KEY } from "../../utils/constants";
 
 const AdminPrivateRouter = (props) => {
   let customerData = JSON.parse(localStorage.getItem(USER_KEY));
-  return customerData && customerData?.role === 1 ? (
-    <Navigate to="/" />
-  ) : customerData && customerData.role === 2 ? (
+  return customerData && customerData.role === 2 ? (
     <AdminLayout {...props} />
   ) : (
     <Navigate to="/admin/login" />
